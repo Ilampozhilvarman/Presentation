013cbbeab783ad0856935e83a98ae468e5758de9
Ilampozhilvarman <Ilampozhilvarman@gmail.com>
2026-06-15 11:47:13 -0500

    Files connected and an elemSelected() function addded.

--- make.js
+++ make.js
@@ -1,25 +1,37 @@
 /*
 command list:
-command n: new elem
+command m: new elem
 arrow key: move elem
 command b: send elem back
 command f: send elem front
-command shift b: send all the way back
-command shift f: send all the way front
+command v: send all the way back
+command g: send all the way front
+command d: download
 */
 
 let elemSelected = null;
+function elemClick(elem) {
+    elemSelected = elem;
+    alert("E");
+}
 document.addEventListener("keydown", function(ev) {
     const modifer = ev.metaKey || ev.ctrlKey;
     if (modifer) {
-        if (ev.key.toLowerCase() === "n") {
+        if (ev.key.toLowerCase() === "m") {
             ev.preventDefault();
             let newElem = prompt("What new element do you want: ");
             if (newElem === "box") {
                 let newBox = document.createElement("div");
                 newBox.classList.add("box");
+                newBox.style.width = "10%";
+                newBox.style.height = "10%";
+                newBox.setAttribute("onclick", "elemSelected(this)");
                 document.body.appendChild(newBox);
             }
         }
+        else if (ev.key.toLowerCase() === "d") {
+
+        }
+        else if (ev.key.toLowerCase() === "")
     }
 });
